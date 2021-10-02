0c71e1d54e6796e2bea5cd82b090d1b193019816
CodigoByte2020 <gmcontrpuma@gmail.com>
2021-10-02 13:58:07 -0500

    [IMP] Creación de módulo ventas

---
+++ ventas/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Ventas',
+    'summary': 'Módulo de Ventas',
+    'description': 'Módulo para la gestión de ventas.',
+    'author': 'Contreras Pumamango Gianmarco - gmcontrpuma@gmail.com',
+    'website': 'https://github.com/CodigoByte2020',
+    'category': 'Tools',
+    'version': '13.0.0.0.1',
+    'depends': [
+        'productos'
+    ],
+    'data': [
+        'views/ventas_menus_views.xml',
+        'views/ventas_cliente_views.xml'
+    ],
+    'installable': True,
+}
