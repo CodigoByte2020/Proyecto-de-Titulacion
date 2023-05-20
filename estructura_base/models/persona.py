cf9251923ed6ddfbbed4a1e27f909ffd1711e921
CodigoByte2020 <gmcontrpuma@gmail.com>
2023-05-20 12:41:50 -0500

    [IMP] Consultar datos de cliente

--- estructura_base/models/persona.py
+++ estructura_base/models/persona.py
@@ -1,6 +1,10 @@
+import logging
 import re
+import requests
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError
+
+_logger = logging.getLogger(__name__)
 
 TIPO_DOCUMENTO_SELECTION = [
     ('dni', 'DOCUMENTO NACIONAL DE IDENTIDAD'),
@@ -17,7 +21,8 @@ class Persona(models.Model):
     _name = 'base.persona'
     _description = 'Clientes y Proveedores'
 
-    name = fields.Char(string='Nombre', required=True)
+    # COMPROBAR QUE LOS CLIENTES SIN NOMBRE NO GENEREN PROBLEMAS EN LOS DEMÁS PROCESOS
+    name = fields.Char(string='Nombre')
     tipo_documento = fields.Selection(TIPO_DOCUMENTO_SELECTION, string='Tipo de Documento', required=True)
     numero_documento = fields.Char(string='Número de documento', required=True)
     direccion = fields.Text(string='Dirección')
@@ -47,4 +52,31 @@ class Persona(models.Model):
                 raise ValidationError('El Número de documento debe tener 11 números.')
 
     def name_get(self):
-        return [(rec.id, f'{rec.name} - {rec.numero_documento}')for rec in self]
+        return [(rec.id, f'{rec.name} - {rec.numero_documento}') for rec in self]
+
+    def consult_data(self):
+        url = f'https://api.apis.net.pe/v1/{self.tipo_documento}'
+        args = {'numero': self.numero_documento}
+        # TRY: CÓDIGO SUCEPTIBLE A ERROR
+        try:
+            response = requests.get(url=url, params=args)
+        # CAPTURAMOS LA EXEPCIÓN
+        except requests.exceptions.RequestException as e:
+            _logger.info(f'*********************** LA CONEXIÓN FALLO *********************** {e}')
+            raise ValidationError('La conexión falló al consultar datos.')
+        # SI NO OCURRE NINGUNA EXEPCIÓN
+        else:
+            if response.status_code == 200:
+                response_json = response.json()
+                nombre = response_json.get('nombre', '')
+                direccion = response_json.get('direccion', '')
+                provincia = response_json.get('provincia', '')
+                self.update({
+                    'name': nombre,
+                    'direccion': direccion and provincia and f'{direccion}\n{provincia} - PERÚ',
+                })
+            elif response.status_code == 422:
+                raise ValidationError(f'El Número de documento {self.numero_documento} no cumple con las reglas de '
+                                      f'validación')
+            elif response.status_code == 404:
+                raise ValidationError(f'El Número de documento {self.numero_documento} no existe')
